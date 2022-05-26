a12e599bd937193363bd70b2073422521c8a3165
Manuel Bazzichi <bazzichimanuel@gmail.com>
2022-05-26 19:30:40 +0200

    feat: card-shared 1.0

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CardShared from "./components/CardShared";
+
+
+const App = () => {
+    return (
+      <div>
+        <CardShared />
+      </div>
+    );
+  };
+  
+  export default App;
--- src/index.js
+++ src/index.js
@@ -10,9 +10,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App username= 'manuel'
+    />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
