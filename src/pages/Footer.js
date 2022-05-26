485336143684cecbfe5620b48018d0513f02b7d4
EnzoSt <Stellavatoe@gmail.com>
2022-05-26 18:13:27 +0200

    first-commit

---
+++ src/pages/Footer.js
@@ -0,0 +1,11 @@
+import "./Navbar-Footer.css"
+
+export function Footer() {
+    return (
+      <nav className="navbar fixed-bottom navbar-light bg-light">
+      <div className="container-fluid">
+        <span className="navbar-brand mb-0 h1"></span>
+      </div>
+    </nav>
+    )
+  }
---
+++ src/pages/Navbar.js
@@ -0,0 +1,11 @@
+import "./Navbar-Footer.css"
+
+export function Navbar() {
+    return (
+      <nav className="navbar bg-light">
+      <div className="container-fluid">
+        <span className="navbar-brand mb-0 h1">E-Fruits</span>
+      </div>
+    </nav>
+    )
+  }
