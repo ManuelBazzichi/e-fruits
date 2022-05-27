07f6e75dace775900b1eedc109ea6470e4359678
EnzoSt <Stellavatoe@gmail.com>
2022-05-27 10:21:31 +0200

    first-commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Footer } from "./pages/Footer";
+import { Navbar } from "./pages/Navbar";
+
+const App = () => {
+
+    return (
+      <div>
+        <Navbar />
+        <Footer />
+      </div>
+    );
+  };
+  
+  export default App;
---
+++ src/pages/Navbar.js
@@ -0,0 +1,33 @@
+import "./Navbar-Footer.css";
+import Logo from "../assets/logo.png";
+import Cart from "../assets/cart.png";
+
+export function Navbar() {
+  return (
+    <nav className="navbar d-flex flex-nowrap">
+      <div className="container-fluid">
+        <a className="navbar-brand" href=" # ">
+          <img
+            src={Logo}
+            width={30}
+            height={30}
+            className="d-inline-block align-top mx-2"
+            alt="Logo"
+          />
+          <span className="navbar-brand h1">E-Fruits</span>
+        </a>
+      </div>
+      <div className="container-fluid justify-content-end">
+        <a href=" # ">
+          <img
+            src={Cart}
+            width={26}
+            height={26}
+            className="d-inline-block align-top me-4"
+            alt="Logo"
+          />
+        </a>
+      </div>
+    </nav>
+  );
+}
