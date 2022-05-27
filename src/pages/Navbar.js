e8f8c767461ca6bdf23a224534af1a9aa0d25a72
EnzoSt <Stellavatoe@gmail.com>
2022-05-27 09:16:00 +0200

    add-assets-folder

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,11 +1,33 @@
-import "./Navbar-Footer.css"
+import "./Navbar-Footer.css";
+import Logo from "../assets/logo.png";
+import Cart from "../assets/cart.png";
 
 export function Navbar() {
-    return (
-      <nav className="navbar bg-light">
+  return (
+    <nav className="navbar d-flex flex-nowrap">
       <div className="container-fluid">
-        <span className="navbar-brand mb-0 h1">E-Fruits</span>
+        <a className="navbar-brand" href=" # ">
+          <img
+            src={Logo}
+            width={30}
+            height={30}
+            className="d-inline-block align-top mx-2"
+            alt="Logo"
+          />
+          <span className="navbar-brand h1">E-Fruits</span>
+        </a>
+      </div>
+      <div className="container-fluid justify-content-end">
+        <a href=" # ">
+          <img
+            src={Cart}
+            width={26}
+            height={26}
+            className="d-inline-block align-top me-4"
+            alt="Logo"
+          />
+        </a>
       </div>
     </nav>
-    )
-  }
+  );
+}
