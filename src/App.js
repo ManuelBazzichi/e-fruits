7228e2c44158011771348ae7ae2c4432d62da115
Dennis <dens90@hotmail.it>
2022-05-31 08:37:10 +0200

    create-Homepage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import CardShared from "./components/CardShared";
 import { Footer } from "./pages/Footer";
+
 import { Navbar } from "./pages/Navbar";
 
 
 const App = () => {
     return (
       <div>
+        
         <Navbar />
         <CardShared />
         <Footer />
+        
       </div>
     );
   };
---
+++ src/pages/Homepage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Footer } from './Footer'
+import { Navbar } from './Navbar'
+
+const Homepage = () => {
+
+    return(
+        <div>
+            <Navbar />
+            <Footer />
+            casdasf
+        </div>
+    )
+}
+
+export default Homepage
