a098a4e03e98e351375e13edeb3b66ead2f6b796
EnzoSt <Stellavatoe@gmail.com>
2022-06-01 20:23:37 +0200

    fixed-responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,7 @@
-import "./Navbar-Footer.css"
+import "./Navbar-Footer.css";
 
 function Footer() {
-    return (
-      <nav className="fixed-bottom  footer">
-      <div className="container-fluid navbar-brand mb-0 h1">
-      </div>
-    </nav>
-    )
-  }
+  return <div className="Footer"></div>;
+}
 
-  export default Footer;
+export default Footer;
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,16 +2,14 @@ import React from "react";
 // import Logo from "../assets/logo.png";
 // import { useFetchData } from "../Hooks/useFetchData";
 import "./Modal.css";
-import { Button, Modal } from "react-bootstrap";
-
+import { Modal } from "react-bootstrap";
 
 const Detail = ({ data, handleClose }) => {
   return (
     <Modal.Dialog className="position-fixed">
-      {/* <Modal.Body> */}
       <div className="d-flex">
-        <div className="flex-grow-1 modal-img">
-          <img src={data.image} width={250} height={250} alt="Fruit" />
+        <div className="flex-grow-1">
+          <img className="modal-img" src={data.image} alt="Fruit" />
         </div>
         <div className="p-4 flex-grow-1 modal-info">
           <h5 className="modal-title">{data.name}</h5>
@@ -33,10 +31,9 @@ const Detail = ({ data, handleClose }) => {
           </button>
         </div>
       </div>
-      <div className="p-2 modal-price  ">
-        <p className="fw-semibold ms-3 fw-bold">$ {data.price}</p>
+      <div className="p-2 modal-price">
+        <p className="fw-semibold ms-4 fw-bold fs-5">$ {data.price}</p>
       </div>
-      {/* </Modal.Body> */}
 
       <div className="modal-footer d-flex justify-content-between">
         <button
@@ -52,59 +49,6 @@ const Detail = ({ data, handleClose }) => {
       </div>
     </Modal.Dialog>
   );
-
-  // return (
-  //     <div className="position-fixed modal">
-  //       <div
-  //         className="modal fade"
-  //         tabIndex={-1}
-  //         role="dialog"
-  //         aria-labelledby="exampleModalLabel"
-  //       >
-  //         <div className="modal-dialog" role="document">
-  //           <div className="modal-content">
-  //             <div className="d-flex">
-  //               <div className="flex-grow-1 modal-img">
-  //                 <img src={data.image} width={250} height={250} alt="Fruit" />
-  //               </div>
-  //               <div className="p-4 flex-grow-1">
-  //                 <h5 className="modal-title">{data.name}</h5>
-  //                 <p>
-  //                   This is some content from a media component. You can replace
-  //                   this with any content and adjust it as needed.
-  //                 </p>
-  //               </div>
-  //               <div className="flex-grow-0">
-  //                 <button
-  //                   onClick={() => handleClose()}
-  //                   type="button"
-  //                   className="close"
-  //                   aria-label="Close"
-  //                 >
-  //                   <span aria-hidden="true">&times;</span>
-  //                 </button>
-  //               </div>
-  //             </div>
-  //             <div className="p-2 modal-price  ">
-  //               <p className="fw-semibold">Price ...</p>
-  //             </div>
-  //             <div className="modal-footer d-flex justify-content-between">
-  //               <button
-  //                 type="button"
-  //                 className="btn btn-secondary button-style-red"
-  //                 // onClick={props.handleClose}
-  //               >
-  //                 Chiudi
-  //               </button>
-  //               <button type="button" className="btn btn-primary button-style">
-  //                 Acquista
-  //               </button>
-  //             </div>
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </div>
-  // );
 };
 
 export default Detail;
