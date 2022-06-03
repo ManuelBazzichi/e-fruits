e82bd10b03dad98bc8c34c67604e5b8c4710f875
Manuel Bazzichi <bazzichimanuel@gmail.com>
2022-06-03 10:09:08 +0200

    finished cart modal

--- src/components/ModalCart.js
+++ src/components/ModalCart.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import "./Modal.css";
 import { Modal } from "react-bootstrap";
 import { CartContext } from "../App";
+import "./ModalCart.css";
 
 const ModalCart = ({ data, handleClose }) => {
   const { cart, onAdd, onRemove } = useContext(CartContext);
@@ -9,25 +10,37 @@ const ModalCart = ({ data, handleClose }) => {
 
   return (
     <Modal.Dialog className="position-fixed">
-      <div className="d-flex ">
-        <div className="flex-grow-1">
-          <img className="modal-img" src={data.image} width={90} alt="Fruit" />
-        </div>
-        <div className="flex-grow-1 modal-info">
-         
-          <h5 className="modal-title mt-2">{data.name}</h5>
-        </div>
-        <div className="p-2 modal-price">
-          <p className="fw-semibold fw-bold fs-5 mt-4" >$ {data.price}</p>
-        </div>
-        <div className="flex-grow-1 p-2 d-flex justify-content-end d-flex align-items-center">
+      {cart.map((el, i) => (
+        <div key={i} className="d-flex ">
+          <div className="flex-grow-1">
+            <img
+              className="modal-img"
+              src={el.image}
+              width={90}
+              alt="Fruit"
+            />
+          </div>
+          <div className="flex-grow-1 modal-info">
+            <h5 className="modal-title mt-2">{el.name}</h5>
+          </div>
+          <div className="p-2 modal-price">
+            <p className="fw-semibold fw-bold fs-5 mt-4">$ {el.price}</p>
+          </div>
+          {/* <div className="flex-grow-1 p-2 d-flex justify-content-end d-flex align-items-center">
         <p className="fw-semibold fw-bold fs-2">+</p>
+        </div> */}
+          <div className="flex-grow-1 p-2 d-flex align-ite justify-content-start d-flex align-items-center">
+            <button
+              className="modal-button fw-semibold fw-bold fs-2 border-0 bg"
+              onClick={() => onRemove(el.id)}
+            >
+              {" "}
+              -{" "}
+            </button>
+          </div>
+          {/* <div className="flex-grow-0"></div> */}
         </div>
-        <div className="flex-grow-1 p-2 d-flex align-ite justify-content-start d-flex align-items-center">
-        <p className="fw-semibold fw-bold fs-1 ">-</p>
-        </div>
-        {/* <div className="flex-grow-0"></div> */}
-      </div>
+      ))}
 
       <div className="modal-footer d-flex justify-content-center">
         <button
@@ -40,7 +53,7 @@ const ModalCart = ({ data, handleClose }) => {
         {/* <button
           type="button"
           className="btn btn-primary"
-          onClick={() => onAdd(data)}
+          onClick={() => onAdd(el)}
         >
           Acquista
         </button> */}
